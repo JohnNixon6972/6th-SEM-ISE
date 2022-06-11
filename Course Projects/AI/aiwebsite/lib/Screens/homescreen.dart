2463f51845f6f0b3b9aa95f26dfc334ac4c8b6b1
Hrutuja Patnekar <hrutujapatnekar22@gmail.com>
2022-06-11 16:14:05 +0530

    added ai website code

---
+++ Course Projects/AI/aiwebsite/lib/Screens/homescreen.dart
@@ -0,0 +1,235 @@
+import 'dart:convert';
+import 'dart:html';
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:aiwebsite/Screens/mainFunction.dart';
+import 'package:aiwebsite/constants.dart';
+import 'package:aiwebsite/main.dart';
+
+// import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+// import 'package:image_picker/image_picker.dart';
+
+class homeScreen extends StatefulWidget {
+  static const String id = 'homeScreen';
+
+  @override
+  State<homeScreen> createState() => _homeScreenState();
+}
+
+// List<XFile>? imageFileList = [];
+
+Image img = Image.asset('uploadImage.png');
+
+class _homeScreenState extends State<homeScreen> {
+  late Uint8List uploadedImage;
+
+  startFilePicker() async {
+    FileUploadInputElement uploadInput = FileUploadInputElement();
+    uploadInput.click();
+    uploadInput.onChange.listen((e) {
+      // read file content as dataURL
+      final files = uploadInput.files;
+      if (files!.length == 1) {
+        final file = files[0];
+        FileReader reader = FileReader();
+        reader.onLoadEnd.listen((e) {
+          setState(() {
+            uploadedImage = reader.result as Uint8List;
+            img = uploadedImage as Image;
+          });
+        });
+        reader.onError.listen((fileEvent) {
+          setState(() {
+            var option1Text = "Some Error occured while reading the file";
+            img = uploadedImage as Image;
+          });
+        });
+
+        setState(() {
+          var _base64 = "/9j/4AAQSkZJRg...";
+          uploadedImage = base64Decode(_base64);
+          img = Image.memory(uploadedImage);
+        });
+      }
+    });
+  }
+
+  final Shader linerGradient = const LinearGradient(
+    colors: <Color>[Color(0xffDA44bb), Color(0xff8921aa)],
+  ).createShader(
+    Rect.fromPoints(Offset(0, 500), Offset(1000, 500)),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 15,
+        toolbarHeight: 50,
+        title: Center(
+          child: Text(
+            "Image Colorization",
+            style: GoogleFonts.cantataOne(
+              textStyle: const TextStyle(
+                  color: kColor2, fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        backgroundColor: kColor3,
+      ),
+      body: Center(
+        child: SafeArea(
+          child: Stack(
+            children: [
+              Opacity(
+                opacity: 1,
+                child: Center(
+                  child: Image.asset(
+                    'images/backgroundimage3.webp',
+                    fit: BoxFit.cover,
+                    height: double.infinity,
+                    width: double.infinity,
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Container(
+                      height: MediaQuery.of(context).size.height,
+                      width: (3 * MediaQuery.of(context).size.width) / 4,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const SizedBox(
+                            height: 50,
+                          ),
+                          Text(
+                            "Let's get started !",
+                            style: GoogleFonts.cantataOne(
+                              textStyle: TextStyle(
+                                // color: kColor2,
+                                fontSize: 50,
+                                fontWeight: FontWeight.bold,
+                                foreground: Paint()..shader = linerGradient,
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 30,
+                          ),
+                          Text(
+                            "DESCRIPTION cnkjsdnv ncdkn dsjkcz, ",
+                            style: GoogleFonts.cantataOne(
+                              textStyle: TextStyle(
+                                // color: kColor2,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                foreground: Paint()..shader = linerGradient,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    const Spacer(),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: MediaQuery.of(context).size.height - 50,
+                          width: (MediaQuery.of(context).size.width) / 4,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: kColor4),
+                          ),
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                SizedBox(
+                                  width: 200,
+                                  child: ElevatedButton(
+                                    onPressed: () async {
+                                      startFilePicker();
+                                    },
+                                    style: ElevatedButton.styleFrom(
+                                      alignment: Alignment.center,
+                                      primary: kColor2,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                    ),
+                                    child: Center(
+                                      child: Text(
+                                        'Upload an Image',
+                                        style: GoogleFonts.cantataOne(
+                                          textStyle: const TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 20),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                Container(
+                                  child: Opacity(
+                                      opacity: 0.3,
+                                      child: Image.asset(
+                                        'images/uploadImage.png',
+                                      )),
+                                  height: 3 *
+                                      ((MediaQuery.of(context).size.width) /
+                                          4) /
+                                      4,
+                                  width: 3 *
+                                      ((MediaQuery.of(context).size.width) /
+                                          4) /
+                                      4,
+                                  decoration: BoxDecoration(
+                                    border: Border.all(color: kColor1),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  height: 20,
+                                ),
+                                ElevatedButton(
+                                  onPressed: () async {
+                                    Navigator.pushNamed(
+                                        context, mainAiFunction.id);
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    primary: kColor2,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(20),
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'Next',
+                                    style: GoogleFonts.cantataOne(
+                                      textStyle: const TextStyle(
+                                          color: Colors.white, fontSize: 20),
+                                    ),
+                                  ),
+                                ),
+                              ]),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ Course Projects/AI/aiwebsite/lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/cupertino.dart';
+
+const kColor1 = Color(0xff9A0680);
+const kColor2 = Color(0xff79018C);
+const kColor3 = Color(0xff4C0070);
+const kColor4 = Color(0xff160040);
